0f426e33e04d7d7d1bc12ce825a935229292d18b
Cassandra K <ckk30@hawaii.edu>
2023-10-19 12:45:23 -1000

    completed part 2

--- Hackwich 8/ViewController.swift
+++ Hackwich 8/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.text = "My Segmented Control"
     }
 
 
+   
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+    }
 }
 
